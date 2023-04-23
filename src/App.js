a7b24cf27951e6263a5c513da39fe0eb5cafe5d6
kyoungholee <dlrudgh1303@gmail.com>
2023-04-23 19:22:44 +0900

    feat: 차트페이지, 상세페이지 라우터 연결

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import ChartPage from './pages/ChartPage';
+import DetailPage from './pages/DetailPage';
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <div className='App'>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<ChartPage />}></Route>
+					<Route path="/detail/:id" element={<DetailPage />}></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
   );
 }
 
